3147a78208446b99318415a118820c3fff6da7d3
ferndz <nafernandez@mi.unc.edu.ar>
2024-03-16 23:56:07 -0300

    Estructura mejorada

--- EstructuraGrafo24.h
+++ EstructuraGrafo24.h
@@ -5,15 +5,17 @@
 typedef unsigned int u32;
 typedef u32 color;
 
-struct vertices{
-    u32** vecinos; // vecinos[i][j] = el vecino j-ésimo del vértice i
-    u32 tamaño;
-};
+typedef struct _vertices{
+    u32 grado;
+    color color;
+} vertices;
 
 typedef struct _Grafo {
-    struct vertices* vertices;
+    vertices* list_vertices;
+    u32** vecinos;
+    u32 cantidad_vertices;
+    u32 cantidad_lados;
     u32 delta;
-    color* list_color;
 } GrafoSt;
 
 #endif
