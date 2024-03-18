cc2d464fc32b2ed0e278607fd4a03683ae7e7734
ferndz <nafernandez@mi.unc.edu.ar>
2024-03-18 12:07:38 -0300

    probando colores

--- tests/test_1.c
+++ tests/test_1.c
@@ -9,6 +9,21 @@ int main()
 
     u32 delta = Delta(g);
     printf("delta: %u\n", delta);
+
+    color c = 1;
+    for(u32 i = 0; i < NumeroDeVertices(g); ++i) {
+        AsignarColor(c,i,g);
+        c++;
+    }
+
+    printf("Los colores de los primeros vertices son: \n");
+    u32 cota_vertice = NumeroDeVertices(g);
+    if(cota_vertice > 10) {
+        cota_vertice = 10;
+    }
+    for(u32 i = 0; i < cota_vertice; ++i ){
+        printf("vertice %u, color %u\n", i, Color(i,g));
+    }
     DestruirGrafo(g);
     return 0;
 }
