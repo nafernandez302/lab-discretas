547a23368b9045ac2874008717c2d670b02199ca
LucianoRojo <lucianorojo064@mi.unc.edu.ar>
2024-04-30 22:44:16 -0300

    Hechos los ordenes

--- API2024Parte2.h
+++ API2024Parte2.h
@@ -6,7 +6,7 @@
 
 bool biyectivo(u32* arr, u32 tam);
 u32 Greedy(Grafo G, u32* Orden);
-
+void quickSortGrado(u32 arr[], u32 low, u32 high, Grafo G);
 char GulDukat(Grafo G, u32* Orden);
 
 char ElimGarak(Grafo G, u32* Orden);
--- main.c
+++ main.c
@@ -4,21 +4,61 @@
 #define N 9
 int main()
 {
-    u32 orden1[N] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-
-    u32 orden2[256];
-    for (size_t i = 0; i < 256; i++)
-    {
-        orden2[i] = i;
-    }
+    /*
     
-   // u32 orden2[N] = {0, 3, 2, 1, 4};
-   // u32 orden3[N] = {4, 3, 0, 1, 2};
-
+    Corremos Greedy en 5 órdenes distintos.
+    1) Orden natural 0, 1, 2, ..., n-1 de los vértices.
+    2) Orden natural inverso n-1, ..., 2, 1, 0 de los vértices.
+    3) Orden primero todos los pares decreciente y los impares creciente.
+    4) Ordenados por grado de mayor a menor.
+    5) Orden a elegir.
+    
+    */
+    
+    // 1) Orden natural de los elementos.
     Grafo G = ConstruirGrafo();
-    u32 cantidad = Greedy(G, orden2);
-    printf("%u\n", cantidad);
-    printf("delta: %u\n", Delta(G));
+    u32 n = NumeroDeVertices(G);
+    u32 Orden1[n];
+    for(u32 i = 0; i < n; i++){
+        Orden1[i] = i;
+    }
+    printf("Cantidad de colores usando el orden 1: %u\n", Greedy(G, Orden1));
+    
+
+    // 2) Orden natural inverso de los elementos.
+    u32 Orden2[n];
+    for(int i = n-1; i >= 0; i--){
+        Orden2[i] = i;
+    }
+    printf("Cantidad de colores usando el orden 2: %u\n", Greedy(G, Orden2));
+    
+    // 3) Orden primero todos los pares decrecientes y después los impares crecientes.
+    u32 Orden3[n];
+    int posicionPar = 0;
+    int posicionImpar = n-1;
+    for(int i = n-1; i >= 0; i--){
+        if(i % 2 != 0){
+            Orden3[posicionImpar] = i;
+            posicionImpar--;
+        } else {
+            Orden3[posicionPar] = i;
+            posicionPar++;
+        }
+    }
+    printf("Cantidad de colores usando el orden 3: %u\n", Greedy(G, Orden3));
+
+    // 4) Orden por grado de mayor a menor.
+    u32 Orden4[n];
+    for(u32 i = 0; i < n; i++){
+        Orden4[i] = i;
+    }
+    quickSortGrado(Orden4, 0, n-1, G);
+    printf("Cantidad de colores usando el orden 4: %u\n", Greedy(G, Orden4));
+
+    // 5) Orden personalizado.
+
+    //TODO
+
     DestruirGrafo(G);
 
     return 0;
